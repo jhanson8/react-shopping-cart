bff5c15a5e885852a5075d80a92123fd145f3cf2
Jonathan Hanson <jhanson8@uoregon.edu>
2020-01-21 15:24:07 -0800

    mvp: added cartcontext and updated nav and carrt components

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ShoppingCart from './components/ShoppingCart';
 
 //Context
 import { ProductContext } from './context/ProductContext';
+import { CartContext } from './context/CartContext';
 
 function App() {
 	const [products] = useState(data);
@@ -21,9 +22,11 @@ function App() {
 
 	return (
 		<div className="App">
+		<ProductContext.Provider value={{ products, addItem }}>
+		<CartContext.Provider value={{cart}}>
 			<Navigation cart={cart} />
 
-<ProductContext.Provider value={{ products, addItem }}>
+
 			{/* Routes */}
 			<Route
 				exact
@@ -34,8 +37,9 @@ function App() {
 
 			<Route
 				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
+					component={ShoppingCart}
 			/>
+			</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
